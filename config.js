dd0632c4a6cd5de4c5264af9208918867fb63f8d
Antonia <abartu00@fesb.hr>
2023-02-23 15:09:58 +0100

    get all users working

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    db: {
+      host: "hci-database.c4lcrwmvgp3y.eu-central-1.rds.amazonaws.com",
+      user: "standard",
+      password: "test",
+      database: "hci",
+    },
+    listPerPage: 10,
+  };
+  module.exports = config;
