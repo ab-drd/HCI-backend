04d49aa994c52055db53c1ad34bb92cbc841779d
Antonia <abartu00@fesb.hr>
2023-02-27 01:14:49 +0100

    fix

--- index.js
+++ index.js
@@ -40,15 +40,10 @@ app.use((err, req, res, next) => {
     return;
 });
 
-const httpServer = http.createServer(app);
 const httpsServer = https.createServer({
   key: fs.readFileSync('key.pem'),
   cert: fs.readFileSync('cert.pem'),
 }, app);
-
-httpServer.listen(80, () => {
-  console.log('HTTP Server running on port 80');
-});
 
 httpsServer.listen(443, () => {
   console.log('HTTPS Server running on port 443');
